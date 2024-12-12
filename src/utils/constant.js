e6e9b94eb93104c8aba3a818fc0e56a919509523
zhuangzi513 <sunyue328@126.com>
2024-12-12 09:50:11 +0800

    Create constant.js

---
+++ src/utils/constant.js
@@ -0,0 +1,25 @@
+/**
+ * 用户 code 换取 session_key
+ * @type {String}
+ */
+export const USER_SPECICAL_INFO = "userSpecialInfo";
+
+/**
+ * 用户信息
+ * @type {String}
+ */
+export const USER_INFO = "userInfo";
+
+/**
+ * 系统信息
+ * @type {String}
+ */
+export const SYSTEM_INFO = "systemInfo";
+
+export const SELECTED_CLASS_CODE = "selectedClassCode"
+
+/**
+ * 搜索历史
+ * @type {Array}
+ */
+export const SEARCH_KEYWORDS = "searchHistoryKeywords"
